a59d58dcd6c1991a95c8cf628842a68bc2d86100
josimar ferreira <josima-tamires@hotmail.com>
2024-07-04 10:52:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const vaiSEHospedar =  confirm('Seja bem vindos(a),galerinha!gostaria de pagar 50,00 para pasar o dia na nossa hospedagem?')
+
+console.log(vaiSEHospedar)
+
+if(vaiSEHospedar){
+    alert('otimo temos as melhores camas de todas regiao')
+}else{
+    alert('que pena! volte sempre')
+}
